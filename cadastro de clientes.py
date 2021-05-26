d33742d08ca68f0e6f229db007e799451a8a73a5
kemuel kesley <kemuelkesleytkd@outlook.com>
2021-05-26 17:40:45 -0300

    Foi criado um campo onde o usuario vai digitar o CPF formatado.

--- cadastro de clientes.py
+++ cadastro de clientes.py
@@ -1,14 +1,18 @@
-print("=====================Cadastro de clientes vers 1.0===================")
+print("=====================Cadastro de clientes versão 1.0===================")
 print()
 nome = str(input("Informe seu nome :"))
 idade = int(input("Informe sua idade :"))
 nascimento = int(input("Informe seu ano de nascimento :"))
-cpf = int(input("Informe seu cpf :"))
+digi_cpf = input("Informe seu CPF sem espaços :")
+
 print("confira as informações.")
 print()
 print("nome :",nome)
 print("idade :",idade)
 print("nascimento:",nascimento)
-print("cpf :",cpf)
-print("Meu nome é %s, eu tenho %d anos, nasci em %d, e meu cpf é: %d" %(nome,idade,nascimento,cpf))
+print("cpf :",digi_cpf)
+
+
+cpf = "{}.{}.{}-{}".format(digi_cpf[:3], digi_cpf[3:6], digi_cpf[6:9], digi_cpf[9:])
+print(cpf)
 
